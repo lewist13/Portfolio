5f01b195fc19f4b8b10e9429c79fdb62dbddd8e7
lewist13 <lewistpro@gmail.com>
2021-10-15 21:56:43 -0400

    styled and added onclick functionality to nav

---
+++ app.js
@@ -0,0 +1,22 @@
+const home = document.querySelector(".home-page");
+const about = document.querySelector(".about-page");
+const skills = document.querySelector(".skills-page");
+const projects = document.querySelector(".projects-page");
+const contact = document.querySelector(".contact-page");
+const hireMe = document.querySelector(".hire-me");
+
+home.addEventListener("click", function () {
+  window.open("https://github.com", "_blank");
+});
+about.addEventListener("click", function () {
+  window.open = "https://github.com";
+});
+skills.addEventListener("click", function () {
+  window.open = "https://github.com";
+});
+projects.addEventListener("click", function () {
+  window.open = "https://github.com";
+});
+contact.addEventListener("click", function () {
+  window.open = "https://github.com";
+});
